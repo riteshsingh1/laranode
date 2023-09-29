34b33767b05f8ef63e45b04bad1e1571d3bc105e
Ritesh Singh <1124ritesh@gmail.com>
2023-09-29 13:12:23 +0530

    Api Docs Updated
    Signed-off-by: Ritesh Singh <1124ritesh@gmail.com>

--- src/app/http/validations/auth.validator.ts
+++ src/app/http/validations/auth.validator.ts
@@ -14,6 +14,22 @@ const validateSignUpRequest = [
     }),
 ];
 
+const validateSignInRequest = [
+  body("username").notEmpty(),
+  body("password").notEmpty(),
+];
+
+const validateForgotPasswordRequest = [body("field").notEmpty()];
+
+const resetPasswordRequest = [
+  body("password").notEmpty().isLength({ min: 8 }),
+  body("otp").notEmpty(),
+  body("token").notEmpty(),
+];
+
 export const validateAuthRequest = {
   validateSignUpRequest,
+  validateSignInRequest,
+  validateForgotPasswordRequest,
+  resetPasswordRequest,
 };
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,26 @@
 import express, { Router } from "express";
 import { authController } from "@/app/http/controllers/auth/auth.controller";
 import { validateAuthRequest } from "@/app/http/validations/auth.validator";
-const { signup } = authController;
-const { validateSignUpRequest } = validateAuthRequest;
+const {
+  signup,
+  resendVerificationRequest,
+  login,
+  forgotPassword,
+  resetPassword,
+} = authController;
+const {
+  validateSignUpRequest,
+  validateForgotPasswordRequest,
+  validateSignInRequest,
+  resetPasswordRequest,
+} = validateAuthRequest;
 const router: Router = express.Router();
 
 router.post("/signup", validateSignUpRequest, signup);
 router.get("/verify", signup);
+router.get("/resend-verification-request", resendVerificationRequest);
+router.post("/signin", validateSignInRequest, login);
+router.post("/forget-password", validateForgotPasswordRequest, forgotPassword);
+router.post("/reset-password", resetPasswordRequest, resetPassword);
 
 export default router;
